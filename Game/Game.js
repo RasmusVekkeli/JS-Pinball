357a1a135a445c5b5b283b60841181b1d3b89491
TeroJokela <tero.jokela10@gmail.com>
2018-03-25 21:44:54 +0300

    Added a base for the flippers

--- Game/Game.js
+++ Game/Game.js
@@ -2,9 +2,9 @@
  *  TODO: [X] walls
  *        [ ] make the ball's physics more realistic
  *        [ ] level
- *			[ ] flippers (player controlled)
- *			[ ] bumpers	(launches the ball when it hits these)
- *			[ ] targets (gives points when hit by ball)
+ *		  [ ] flippers (player controlled)
+ *		  [ ] bumpers (launches the ball when it hits these)
+ *		  [ ] targets (gives points when hit by ball)
  *        [ ] score
  *        [/] plunger
  *        [ ] misc. game logic (game over, lost ball etc...)
@@ -15,6 +15,7 @@ var ground;
 var keys;
 var helloText;
 var walls = [];
+var flipperMovementSpeed = 0.30;
 
 // Initialize
 function setup() {
@@ -25,11 +26,16 @@ function setup() {
         frictionAir: 0.0,
         friction: 0.0,
 		density: 10,
-		restitution: 0.4
+		restitution: 0.8
     }
 
 	ball = matter.makeBall(random(0, width), 300, 40, ballOptions);
 	ground = matter.makeBarrier(width / 2, 900, 1000, 15);
+
+    leftFlipper = matter.makeBarrier(width / 2 - 50, 300, 75, 10);
+    rightFlipper = matter.makeBarrier(width / 2 + 50, 300, 75, 10);
+    leftFlipper.setAngle(-0.05);
+    rightFlipper.setAngle(0.05);
 
     textOptions = {
         isStatic: true,
@@ -62,6 +68,28 @@ function moveGround() {
 	if (keys && keys[32]) { plunge(); }											// Activate plunger
 }
 
+function moveFlippers() {
+    if (keys && keys[37]) {
+        if (leftFlipper.getAngle() > -0.5) {
+            leftFlipper.setAngle(leftFlipper.getAngle() - flipperMovementSpeed);
+        }
+    } else {
+        if (leftFlipper.getAngle() < 0.5) {
+            leftFlipper.setAngle(leftFlipper.getAngle() + flipperMovementSpeed);
+        }
+    }
+
+    if (keys && keys[39]) {
+        if (rightFlipper.getAngle() < 0.5) {
+            rightFlipper.setAngle(rightFlipper.getAngle() + flipperMovementSpeed);
+        }
+    } else {
+        if (rightFlipper.getAngle() > -0.5) {
+            rightFlipper.setAngle(rightFlipper.getAngle() - flipperMovementSpeed);
+        }
+    }
+}
+
 // Doesn't work set the ball's position, please fix
 function checkBall() {
     if (ball.isOffCanvas()) {
@@ -86,7 +114,13 @@ function draw() {
     // Draw the ball
     checkBall();
 	fill(70);
-	ball.show();
+    ball.show();
+
+    // Draw the flippers
+    fill(255, 0, 255);
+    moveFlippers();
+    leftFlipper.show();
+    rightFlipper.show();
 
     // Draw the walls
     fill(140);
